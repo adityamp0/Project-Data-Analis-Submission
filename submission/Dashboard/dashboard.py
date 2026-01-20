7c6f730eef95bcec2aba4c9da54b710863dadeec
Aditya Maulana Pamungkas <100006466+adityamp0@users.noreply.github.com>
2026-01-20 23:07:23 +0700

    Change data file from 'all_data.csv' to 'main_data.csv'

--- submission/Dashboard/dashboard.py
+++ submission/Dashboard/dashboard.py
@@ -50,7 +50,7 @@ sns.set_palette("bright")
 @st.cache_data
 def load_data():
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(current_dir, "all_data.csv")
+    file_path = os.path.join(current_dir, "main_data.csv")
     df = pd.read_csv(file_path)
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     return df
